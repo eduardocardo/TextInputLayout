d4d9359b6260644682ff3eff535c65b455783fa0
eduardocardo <eduardocardomartinez@gmail.com>
2016-04-19 00:12:09 +0200

    Añadido metodo alertagps

--- app/src/main/java/com/educar/textinputlayout/MapActivity.java
+++ app/src/main/java/com/educar/textinputlayout/MapActivity.java
@@ -1,17 +1,22 @@
 package com.educar.textinputlayout;
 
 import android.Manifest;
+
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,6 +44,7 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
     private boolean modoAutomatico;
     private FloatingActionButton fabAuto;
     private FloatingActionButton fabManual;
+    private AlertDialog  alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,28 +59,14 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
         fabManual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Snackbar.make(v, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(v, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
             }
 
         });
 
-       /* if (ContextCompat.checkSelfPermission(this, Manifest.permission.
-                ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mapa.setMyLocationEnabled(true);
-        }*/
 
-       // mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(UPV, 15));
-       /* mapa.getUiSettings().setZoomControlsEnabled(false);
-        mapa.getUiSettings().setCompassEnabled(true);
-        mapa.addMarker(new MarkerOptions()
-                .position(UPV)
-                .title("UPV")
-                .snippet("Universidad Politécnica de Valencia")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(android.R.drawable.ic_menu_compass))
-                .anchor(0.5f, 0.5f));*//*
 
-*/
+
     }
 
     @Override
@@ -90,12 +82,13 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapa = googleMap;
+        mapa.setMyLocationEnabled(true);
        /* mapa.addMarker(new MarkerOptions()
                 .position(new LatLng(latitud, longitud))
                 .title("Marker"))*/;
 
            /*Se comprueba la versión del dispositivo en que se lanza la aplicación.Si la versión del dispositivo es mayor o igual a la 6.0 en clave*/
-
+/*
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //Comprobamos si tenemos los permisos de localización exigidos, de no tenerlos se solicitan al usuario
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -107,12 +100,12 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
         } else {
             //Si la version es menor a la 6.0 directamete se continua invocando al metodo rastreoGPS().
             rastreoGPS();
-        }
-        rastreoGPS();
+        }*/
+
         mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mapa.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitud, longitud)));
-
-        Log.d("TAGLONGITUD",String.valueOf(longitud));
+        rastreoGPS();
+        Log.d("TAGLONGITUD", String.valueOf(longitud));
         Log.d("TAGLATITUD",String.valueOf(latitud));
         mapa.setOnMapClickListener(this);
     }
@@ -140,7 +133,7 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
         // Comprobamos si está disponible el proveedor GPS.
         if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
         {
-            Toast.makeText(MapActivity.this, "Proveedor de gps desactivado", Toast.LENGTH_SHORT).show();
+            alertaNoGps();
         }
         //Se llama al método que muestra la posición
          mostrarPosicion(loc);
@@ -179,7 +172,7 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
          - y la variable de tipo LocationListener
         que actualizará la localización en caso de producirse nuevos cambios.*/
 
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0,locListener);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
 
     }
 
@@ -191,6 +184,10 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
             longitud = loc.getLongitude();
             Log.d("TAGLONGITUD",String.valueOf(longitud));
             Log.d("TAGLATITUD",String.valueOf(latitud));
+            mapa.addMarker(new MarkerOptions()
+                    .position(new LatLng(latitud, longitud))
+                    .title("Marker"));
+            mapa.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(latitud,longitud)));
         }
     }
 
@@ -214,9 +211,73 @@ public class MapActivity extends FragmentActivity implements GoogleMap.OnMapClic
                 } else {
                     //Permiso denegado..
                     //Snackbar.make(vista, "Permiso denegado, no se podrá actualizar la posicion del GPS", Snackbar.LENGTH_LONG).show();
+                    return;
                 }
-                return;
+
+
         }
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Y si no tiene los permisos necesarios
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            else
+            {
+                locManager.removeUpdates(locListener);
+            }
+        }
+        else
+        {
+            locManager.removeUpdates(locListener);
+        }
+    }
+
+   /* @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Y si no tiene los permisos necesarios
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            else
+            {
+                locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+            }
+        }
+        else
+        {
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+        }
+
+    }*/
+
+    public void alertaNoGps()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("El GPS esta desactivado,Deseas activarlo?")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alert = builder.create();
+        alert.show();
     }
 
 }
